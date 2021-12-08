3e6a656887e60f3111750880d4dc47c7f20f0d8f
Andreas Møller Laursen <Fyziikme@gmail.com>
2021-12-09 00:56:24 +0100

    Minor fix to day 1

--- day_1/run.py
+++ day_1/run.py
@@ -1,4 +1,4 @@
-import json, sample
+import json, sample, time
 
 
 res = json.loads(sample.getData())
@@ -6,10 +6,11 @@ index = 0
 lastValue = -1
 amountIncreased = 0
 for x in res:
-  print(f"Index: {index} value: {x} Increased?: {'(N/A - no previous measurement)' if lastValue == -1 else '(increased)' if x > lastValue else '(decreased)'}")
+  print(f"{x} {'(N/A - no previous measurement)' if lastValue == -1 else '(increased)' if x > lastValue else '(decreased)'}")
   index += 1
+  time.sleep(2)
   if (x > lastValue):
     amountIncreased += 1
   lastValue = x
 
-print(amountIncreased)
+print(amountIncreased - 1)
